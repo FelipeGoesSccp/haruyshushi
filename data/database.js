13b0befd52a8315adfafd62f677f7bb8ce691e3a
FelipeGoesSccp <felipe.g.souza22@aluno.senai.br>
2026-03-03 10:27:36 -0300

    Add files via upload

---
+++ data/database.js
@@ -0,0 +1,30 @@
+let categorias = [
+    { id: 1, nome: 'Temaki' },
+    { id: 2, nome: 'Combindado' },
+    { id: 3, nome: 'Bebidas' },
+    { id: 4, nome: 'Sobremesas' },
+];
+
+let produtos = [
+    {
+        categoriaId: 1,
+        nome: 'Temaki Completo',
+        descricao: "Temaki completo com salmão, cream cheese e cebolinha",
+        preco: 35.00,
+        imagem: 'https://www.seara.com.br/wp-content/uploads/2025/09/temaki-de-salmao-portal-minha-receita.webp'
+    },
+    {
+        categoriaId: 2,
+        nome: 'Combinado 20 Peças',
+        descricao: "Combinado de 20 peças com uma variedade de sushis e sashimis",
+        preco: 120.00,
+        imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu2yEzbyO9fDBERho0VW4F0d0vArBsFgfE0A&s'
+    },
+    {
+        categoriaId: 3,
+        nome: 'Pepsi 2L',
+        descricao: "Bebida gelada",
+        preco: 14.50,
+        imagem: 'https://zaffari.vtexassets.com/arquivos/ids/251532/1006806-00.jpg?v=638561544012470000'
+    },
+]
